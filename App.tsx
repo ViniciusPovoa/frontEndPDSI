a83ff37fced98eaa645885bd46ce36f7adff0c3b
Arthur020104 <arthur.carvalho.oliveira@ufu.br>
2025-09-06 10:31:49 -0300

    UPDATE: login como pagina incial

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Register"
           component={RegisterScreen}
